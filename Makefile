ea0842a126cae3d0ff3fc52ffe42f34bb604c3fa
Your Name <you@example.com>
2024-05-23 03:04:55 +0000

    config

--- Makefile
+++ Makefile
@@ -4,8 +4,31 @@ ROOT := $(shell pwd)
 
 HISTORY := /tmp/docker-bash-history
 
+SOURCES := \
+    Makefile \
+    compile.sh \
+    $(wildcard *.json) \
+
+CONFIGS := \
+  config/jni-config.json \
+  config/resource-config.json \
+  config/reflect-config.json \
 
 default:
+
+run: build
+	target/jna
+
+build: target/jna
+
+target/jna: compile.sh $(CONFIGS) target/jna-0.1.0-standalone.jar
+	bash $<
+
+config/%.json: config/%.yaml
+	ys -J $< > $@
+
+target/jna-0.1.0-standalone.jar: $(SOURCES)
+	clojure -T:build uber
 
 # Try this repo in Docker per the README instructions:
 test:
@@ -22,7 +45,7 @@ test-shell: $(HISTORY)
 	    clojure bash -l
 
 clean:
-	$(RM) -r .cpcache/ target
+	$(RM) -r .cpcache/ target $(CONFIGS)
 
 $(HISTORY):
 	mkdir $@
--- compile.sh
+++ compile.sh
@@ -3,11 +3,17 @@
 set -e
 set -x
 
-native-image --report-unsupported-elements-at-runtime \
-             --no-fallback \
-             -J-Dclojure.compiler.direct-linking=true \
-             -H:ReflectionConfigurationFiles=config/reflect-config.json \
-             -H:JNIConfigurationFiles=config/jni-config.json \
-             -H:ResourceConfigurationFiles=config/resource-config.json \
-             -jar ./target/jna-0.1.0-standalone.jar \
-             -H:Name=./target/jna
+native-image \
+  --no-fallback \
+  --report-unsupported-elements-at-runtime \
+  \
+  -J-Dclojure.compiler.direct-linking=true \
+  \
+  -H:Log=registerResource:5 \
+  \
+  -H:JNIConfigurationFiles=config/jni-config.json \
+  -H:ResourceConfigurationFiles=config/resource-config.json \
+  -H:ReflectionConfigurationFiles=config/reflect-config.json \
+  \
+  -jar target/jna-0.1.0-standalone.jar \
+  -H:Name=target/jna
